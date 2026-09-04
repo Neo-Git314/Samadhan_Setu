70dcd93e4a5331a6359f221559b02e1ffde21e8e
Neo <utkarshsing3.14@gmail.com>
2026-09-04 20:53:18 +0530

    Remove duplicate server directory

--- server/utils/generateJwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export function generateJwt(payload) {
-  const secret = process.env.JWT_SECRET || 'super_secret_jwt_key_for_samadhan_setu_2026';
-  return jwt.sign(payload, secret, { expiresIn: '7d' });
-}
-
-export default generateJwt;
